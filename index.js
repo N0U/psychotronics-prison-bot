60bc1a972595740892d45b7d8dc53e00ec2338f0
banana_Ai <petushkov.egor@gmail.com>
2021-08-19 23:07:30 +0300

    Experement with heroku

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf(process.env.TOKEN);
+const TOKEN = process.env.TOKEN;
+const PROD = process.env.PROD;
+
+const bot = new Telegraf(TOKEN);
 
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.on('new_chat_members', async (ctx) => {
@@ -13,7 +16,22 @@ bot.on('new_chat_members', async (ctx) => {
       console.log('Promoted ' + member.username);
   }
 })
-bot.launch();
+
+if(PROD) {
+  console.log('Launch in prod mode');
+  console.log(process.env);
+  const PORT = process.env.PORT;
+  const URL = process.env.APP_URL;
+  /*bot.launch({
+    webhook: {
+      domain,
+      port: Number(process.env.PORT),
+    }
+  })*/
+} else {
+  console.log('Launch in dev mode');
+  bot.launch();
+}
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
