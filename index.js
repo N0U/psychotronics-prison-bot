701e61b1548f324bd2cfd1bedcd3a530068785cc
banana_Ai <petushkov.egor@gmail.com>
2021-08-20 03:48:04 +0300

    Promote new user on first message

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
-const { Telegraf } = require('telegraf');
+const { Telegraf, Telegram } = require('telegraf');
 
 const bot = new Telegraf(process.env.TOKEN);
 
 bot.start((ctx) => ctx.reply('Welcome'));
+
 bot.on('new_chat_members', async (ctx) => {
   for (const member of ctx.message.new_chat_members) {
+    // Don't promote bots
+    if(member.is_bot) {
+      continue;
+    }
+
     const p = await ctx.promoteChatMember(member.id, {
       is_anonymous: true,
     });
@@ -13,10 +19,27 @@ bot.on('new_chat_members', async (ctx) => {
       console.log('Promoted ' + member.username);
     }
     else {
-      console.console.error('Cannot promote: ' + p);
+      console.error('Cannot promote: ' + p);
     }
   }
 })
+
+bot.on('message', async (ctx) => {
+  const { from, chat, message_id } = ctx.update.message;
+  if(from.is_bot){
+    return;
+  }
+  await ctx.deleteMessage(message_id);
+  const p = await ctx.promoteChatMember(from.id, {
+    is_anonymous: true,
+  });
+  if(p) {
+    console.log('Promoted ' + member.username);
+  }
+  else {
+    console.error('Cannot promote: ' + p);
+  }
+});
 
 if(process.env.PROD) {
   console.log('Launch in prod mode');
