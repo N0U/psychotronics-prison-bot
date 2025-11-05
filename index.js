8fb26fa8399ad8ac149ecf8429727c23da108397
banana_Ai <petushkov.egor@gmail.com>
2025-11-05 22:45:59 +0300

    Updated packages, delete delete command and improves so it works with channels

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Telegraf, Telegram, Markup } = require('telegraf');
+const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.TOKEN);
 
@@ -7,47 +7,27 @@ bot.telegram.setMyCommands([
   { command: 'delete', description: 'Удалить сообщение анонимно' },
 ]);
 
-bot.start((ctx) => ctx.reply('Привет'));
-
-bot.command('delete', async (ctx) => {
-  const message = ctx.message;
-  const { reply_to_message: reply } = message;
-  try {
-    if(!reply) {
-      await ctx.reply('Котик, я не понимаю что удалять. Вызови эту команду когда цитируешь нужное сообщение');
-      return;
-    }
-    if(reply.chat.id !== message.chat.id) {
-      await ctx.reply('Я не буду удалять сообщения из другого чата');
-      return;
-    }
-    await Promise.all([
-      ctx.deleteMessage(reply.message_id),
-      ctx.deleteMessage(message.message_id),
-    ]);
-  } catch (error) {
-    console.log('\tCannot delete message');
-    console.error(error);
-  }
-});
+bot.start((ctx) => ctx.reply('Привет v2 for Channels'));
 
 bot.on('chat_join_request', async (ctx) => {
   try {
     console.log('New join chat_join_request');
     const { chat, from } = ctx.chatJoinRequest
     if (from.is_bot) {
-      console.log('I wont aprove bot');
+      console.log('I wont aprove a clanker');
       return;
     }
     console.log(`${from.first_name} try to join ${chat.title}`);
     await ctx.approveChatJoinRequest(from.id);
-    await ctx.promoteChatMember(from.id, { is_anonymous: true });
+    await ctx.promoteChatMember(from.id, {
+      is_anonymous: true,
+      can_post_messages: true,
+    });
   } catch (error) {
     console.log('\tchat_join_request failed');
     console.error(error);
   }
 });
-
 
 if(process.env.PROD) {
   console.log('Launch in prod mode');
